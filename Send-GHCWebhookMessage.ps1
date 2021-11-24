5517d771c5cd0ea15b387a9f6c4c639a09facc73
Scott Dean <psdean@ncsu.edu>
2021-11-24 14:24:27 -0500

    Added proxy parameters to Send-GHCWebhookMessage.ps1
    Added -proxy and -proxycredential to GHCWebhookMessage.ps1 so the
    Invoke-WebRequest can be sent through a proxy server

--- Send-GHCWebhookMessage.ps1
+++ Send-GHCWebhookMessage.ps1
@@ -8,7 +8,15 @@ function Send-GHCWebhookMessage {
         # Message
         [Parameter(Mandatory)]
         [hashtable]
-        $Message
+        $Message,
+        # Proxy URI
+        [String]
+        $proxy,
+        # Proxy Credential
+        #   Only valid if Proxy parameter is also used
+        [ValidateScript({($null -ne $proxy) })]
+        [pscredential]
+        $ProxyCredential
     )
 
     begin {
@@ -18,7 +26,22 @@ function Send-GHCWebhookMessage {
         $JSON = ConvertTo-Json $Message -Depth 50
         $JSON
 
-        Invoke-WebRequest -Uri $URI -Method POST -Headers @{"Content-Type" = 'Application/json; charset=UTF-8'} -Body $JSON
+        # Build splatting base hashtable
+        $splat=@{
+            URI = $URI
+            Method = 'POST'
+            Headers = @{"Content-Type" = 'Application/json; charset=UTF-8'}
+            Body = $JSON
+        }
+        if ($proxy) {
+            # If proxy parameter specified, add it to the splatting hashtable
+            $splat['Proxy'] = $proxy
+            if ($ProxyCredential) {
+                # if ProxyCredential parameter specified, add it to the splatting hashtable
+                $splat['ProxyCredential'] = $ProxyCredential
+            }
+        }
+        Invoke-WebRequest @splat
     }
 
     end {
